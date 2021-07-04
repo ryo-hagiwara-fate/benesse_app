40f846b1c219acd02cb821e78adedaef5c1d2452
nabe <shiokoshou1197@gmail.com>
2021-07-04 09:10:14 +0900

    ボトムタブ4つに変更

--- lib/high-five/screens/home_screen.dart
+++ lib/high-five/screens/home_screen.dart
@@ -20,19 +20,12 @@ class _HomeScreenState extends State<HomeScreen> {
     YourInfoScreen(),
     FriendsListScreen(),
     QuestionThreadScreen(),
-    StudyPostScreen(),
     StudyThreadScreen()
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   automaticallyImplyLeading: false,
-      //   leading: IconButton(onPressed: () { Navigator.pushNamed(context, HomeScreen.id); }, icon: Icon(Icons.home),),
-      //   title: Text("user details"),
-      //   centerTitle: true,
-      // ),
       body: SafeArea(child: _pageWidgets.elementAt(_currentIndex)),
       bottomNavigationBar: BottomNavyBar(
         items: [
@@ -40,7 +33,6 @@ class _HomeScreenState extends State<HomeScreen> {
           BottomNavyBarItem(icon: Icon(Icons.chat_outlined), title: Text("Chat", style: TextStyle(fontSize: 18),), activeColor: Colors.deepPurple, textAlign: TextAlign.center),
           BottomNavyBarItem(icon: Icon(Icons.catching_pokemon), title: Text("PieChart1", style: TextStyle(fontSize: 18),), activeColor: Colors.deepOrange, textAlign: TextAlign.center),
           BottomNavyBarItem(icon: Icon(Icons.local_drink), title: Text("fl_charts", style: TextStyle(fontSize: 18),), activeColor: Colors.blueGrey, textAlign: TextAlign.center),
-          BottomNavyBarItem(icon: Icon(Icons.handyman), title: Text("First", style: TextStyle(fontSize: 18),), activeColor: Colors.brown, textAlign: TextAlign.center),
         ],
         selectedIndex: _currentIndex,
         onItemSelected: (index) => setState(() => _currentIndex = index),
